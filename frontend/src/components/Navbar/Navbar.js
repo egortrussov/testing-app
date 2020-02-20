e695b889f54b47d636620483069ed65e4949d263
Yegor Trussov <egortr2004@mail,ru>
2020-02-20 19:19:25 +0600

    alpha 0.2.0 final

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -5,6 +5,8 @@ import ls from 'local-storage'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faAlignLeft, faPenAlt, faSignInAlt, faSignOutAlt, faUserPlus } from '@fortawesome/free-solid-svg-icons'
 
+import Logo from './img/logo.svg'
+
 import TestsContext from '../../context/TestsContext'
 
 class Navbar extends Component {
@@ -29,23 +31,28 @@ class Navbar extends Component {
             <nav>
                 <div className="nav-item nav-top">
                     <Link to="/">
-                        <h2 className="logo">EasyTest</h2>
+                        <h2 className="logo"><img src={ Logo } alt=""/> <span className="text">EasyTest</span></h2>
                     </Link>
                 </div>
-                <div className="nav-item nav-middle">
-                    <NavLink exact className="nav-link" to={process.env.PUBLIC_URL+"/app/"}><FontAwesomeIcon className="icon" icon={ faHome } /> Home</NavLink>
-                    <NavLink className="nav-link" to={process.env.PUBLIC_URL+"/app/allTests"}><FontAwesomeIcon className="icon" icon={ faAlignLeft } /> Tests</NavLink>
-                    <NavLink className="nav-link" to={process.env.PUBLIC_URL+"/app/createTest"}><FontAwesomeIcon className="icon" icon={ faPenAlt } /> Create Test</NavLink>
-                </div>
+                { 
+                    isLoggedIn && (
+                        <div className="nav-item nav-middle">
+                            <NavLink exact className="nav-link" to={"/app/"}><FontAwesomeIcon className="icon" icon={ faHome } /> <span className="text">Home</span></NavLink>
+                            <NavLink className="nav-link" to={"/app/allTests"}><FontAwesomeIcon className="icon" icon={ faAlignLeft } /><span className="text"> Tests</span></NavLink>
+                            <NavLink className="nav-link" to={"/app/createTest"}><FontAwesomeIcon className="icon" icon={ faPenAlt } /> <span className="text">Create Test</span></NavLink>
+                        </div>
+                    )
+                }
+                
                 <div className="nav-item nav-bottom">
                     { !isLoggedIn && (
                         <>
-                            <NavLink className="nav-link" to={process.env.PUBLIC_URL+"/app/login"}><FontAwesomeIcon className="icon" icon={ faSignInAlt } /> Login</NavLink>
-                            <NavLink className="nav-link" to={process.env.PUBLIC_URL+"/app/register"}><FontAwesomeIcon className="icon" icon={ faUserPlus } /> Register</NavLink>
+                            <NavLink className="nav-link" to={"/app/login"}><FontAwesomeIcon className="icon" icon={ faSignInAlt } /> <span className="text">Login</span></NavLink>
+                            <NavLink className="nav-link" to={"/app/register"}><FontAwesomeIcon className="icon" icon={ faUserPlus } /> <span className="text">Register</span></NavLink>
                         </>
                     ) }
                     { isLoggedIn && (
-                        <button className="nav-link" onClick={ this.logout.bind(this) } ><FontAwesomeIcon className="icon" icon={ faSignOutAlt } /> Logout</button>
+                        <button className="nav-link" onClick={ this.logout.bind(this) } ><FontAwesomeIcon className="icon" icon={ faSignOutAlt } /> <span className="text">Logout</span></button>
                     ) }                    
                 </div>
             </nav>
