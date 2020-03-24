5f5cac31b8bb39764aae60290f46b7b7fb8fe840
Yegor Trussov <egortr2004@mail.ru>
2020-03-24 13:53:16 +0600

    project restructure

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const path = require('path');
 
 const { DB_URL_DEVELOPMENT, DB_URL_PRODCTION } = require('./config');
 
@@ -11,13 +12,21 @@ app.use(express.json());
 app.use(cors());
 
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello from Express' });
-})
+// app.get('/', (req, res) => {
+//     res.json({ message: 'Hello from Express' });
+// })
 
 app.use('/api/tests', require('./routes/testsRoutes'));
 
 app.use('/api/users', require('./routes/usersRoutes'));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+}
 
 const env = app.get('env');
 const DB_URL = env === 'development' ? DB_URL_DEVELOPMENT : DB_URL_PRODCTION;
