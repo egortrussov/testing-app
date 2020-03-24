e3b7662cbe771e2981050daeddab998338e0f6b7
Yegor Trussov <egortr2004@mail.ru>
2020-03-24 14:11:55 +0600

    heriku deploy 3rd try

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     state = {
         userId: ls.get('userId'),
         token: ls.get('token'),
-        proxy: window.location.href.startsWith('http://') ? 'http://localhost:5000' : 'https://testing-app-easytest.herokuapp.com'
+        proxy: window.location.href.startsWith('http://') ? 'http://localhost:5000' : ''
     }
 
     setToken = (token, userId) => {
