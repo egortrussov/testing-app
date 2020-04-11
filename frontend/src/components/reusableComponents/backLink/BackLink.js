6b4c6b77b9ef3f95bebc4f7feb9a88507a4285bd
Yegor Trussov <egortr2004@mail.ru>
2020-04-11 17:48:11 +0600

    1.0.0 release final

--- frontend/src/components/reusableComponents/backLink/BackLink.js
+++ frontend/src/components/reusableComponents/backLink/BackLink.js
@@ -4,14 +4,16 @@ import { useHistory } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
+import './css/style.css'
+
 const BackLink = ({ link }) => {
     let history = useHistory();
 
     return (
-        <div onClick={ () => history.goBack() } className="page-top">
-            <a href=" ">
+        <div className="page-top">
+            <span onClick={ () => history.goBack() } className="a">
                 <FontAwesomeIcon icon={ faArrowLeft } /> Back 
-            </a>
+            </span>
         </div>
     )
 }
