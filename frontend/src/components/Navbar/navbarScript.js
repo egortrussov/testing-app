dd1acbe4a6b44b292a55faca209b66b1e606eb81
Yegor Trussov <egortr2004@mail.ru>
2020-04-15 21:27:07 +0600

    hide navbar functioonality implemented

--- frontend/src/components/Navbar/navbarScript.js
+++ frontend/src/components/Navbar/navbarScript.js
@@ -1,15 +1,28 @@
-const toggleView = () => {
+import ls from 'local-storage';
+
+const toggleView = (shouldChangeLS) => {
     let nav = document.querySelector('nav');
     let linkTexts = document.querySelectorAll('.text');
     let btn = document.querySelector('#toggle-view');
     nav.classList.toggle('hidden');
     linkTexts.forEach(text => text.classList.toggle('hidden'));
     btn.classList.toggle('hidden');
+    console.log('object')
+
+    if (shouldChangeLS) {
+        let state = ls.get('isNavbarHidden');
+        ls.set('isNavbarHidden', !state);
+    }
 }
 
 const init = () => {
     let btn = document.querySelector('#toggle-view');
-    btn.addEventListener('click', toggleView);
+    const state = ls.get('isNavbarHidden');
+    console.log(state)
+    if (state) {
+        toggleView(false);
+    }
+    btn.addEventListener('click', () => toggleView(true));
 }
 
 export {
